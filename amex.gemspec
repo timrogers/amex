42f2d5542958d3f331451e21f7171aa903283e12
Tim Rogers <tim@gocardless.com>
2012-12-26 11:21:20 +0000

    New changes and gemspec

---
+++ amex.gemspec
@@ -0,0 +1,14 @@
+require File.expand_path('../lib/amex/version', __FILE__)
+
+Gem::Specification.new do |gem|
+  gem.name = 'amex'
+  gem.version = Amex::VERSION.dup
+  gem.authors = ['Tim Rogers']
+  gem.email = ['tim@tim-rogers.co.uk']
+  gem.summary = 'A library for accessing data on an American Express account'
+  gem.homepage = 'https://github.com/timrogers/amex'
+
+  gem.add_runtime_dependency "httparty"
+
+  gem.files = `git ls-files`.split("\n")
+end%
--- example.rb
+++ example.rb
@@ -1,15 +1,5 @@
-# Try to bring in the Amex gem
 begin
-  require 'amex'
-rescue Exception => e
-  puts "Unable to load Amex gem from installed gems"
-  puts "Message: #{e.message}"
-  puts "Backtrace: #{e.backtrace.inspect}"
-end
-
-# ...but if we don't have it, we can  safely attempt to pull it in
-begin
-  Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |f| require(f) }
+  require "#{File.dirname(__FILE__)}/lib/amex.rb"
 rescue Exception => e
   puts "Unable to load Amex gem from lib/amex directory"
   puts "Message: #{e.message}"
@@ -22,3 +12,4 @@ require "#{File.dirname(__FILE__)}/settings.rb"
 
 client = Amex::Client.new(@username, @password)
 account = client.fetch
+puts account.inspect
